e821e324e4795f10971315ceeb3fb3b4ebe79cc6
sreenud <sreenud@outlook.com>
2020-09-19 07:49:02 +0530

    Article css added

--- app/controllers/admin/media_storages_controller.rb
+++ app/controllers/admin/media_storages_controller.rb
@@ -4,7 +4,7 @@ module Admin
       @media_storage = MediaStorage.create(image: params[:attachment])
       if @media_storage
         render(json: { message: "success",
-                       url: @media_storage.image_url,
+                       url: @media_storage.image_url(public: true),
                        attachment_id: @media_storage.id },
          status: :created)
       else
