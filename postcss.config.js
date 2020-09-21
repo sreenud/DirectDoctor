0baeb0d2948df84fc3279db5b30891d88b773c4b
sreenud <sreenud@outlook.com>
2020-09-21 09:08:46 +0530

    Purge css settings

--- postcss.config.js
+++ postcss.config.js
@@ -16,10 +16,16 @@ const environment = {
   ],
 };
 
-if (process.env.RAILS_ENV === 'production_new') {
+if (process.env.RAILS_ENV === 'production') {
   environment.plugins.push(
     require('@fullhuman/postcss-purgecss')({
-      content: ['./app/**/*.html.erb', './app/helpers/**/*.rb'],
+      content: [
+        './app/**/*.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+      ],
+      whitelistPatterns: [/choices/],
+      whitelistPatternsChildren: [],
       defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
     })
   );
