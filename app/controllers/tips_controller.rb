91bfa12f95fb123ea5809572ebbcea9c2c143634
sreenud <sreenud@outlook.com>
2020-10-28 20:25:43 +0530

    Tips listing page is done

---
+++ app/controllers/tips_controller.rb
@@ -0,0 +1,29 @@
+class TipsController < ApplicationController
+  before_action :set_topic
+  before_action :set_tip, only: [:show]
+  before_action :set_meta_data, only: [:show]
+
+  def index
+    @tips = Tip.includes(topic: :author).published
+  end
+
+  def show
+    @other_readings = Tip.where.not(id: @tip.id).published.limit(5)
+  end
+
+  private
+
+  def set_topic
+    @topic = Topic.find_by_slug(params[:topic_id])
+  end
+
+  def set_tip
+    @tip = Tip.find_by_code(params[:id])
+  end
+
+  def set_meta_data
+    @meta_title ||= @tip&.meta_title
+    @meta_description ||= @tip&.meta_description
+    @allow_robots ||= @tip.status == 'published' ? true : false
+  end
+end
