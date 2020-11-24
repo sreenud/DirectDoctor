e08b9692eb64117b6afe899eb95dc7eb77492c86
r11n <nekkanti.raghavendra2@gmail.com>
2020-11-24 11:28:40 +0530

    rubocop offenses

--- spec/components/star_rating_component_spec.rb
+++ spec/components/star_rating_component_spec.rb
@@ -1,6 +1,6 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe StarRatingComponent, type: :component do
+RSpec.describe(StarRatingComponent, type: :component) do
   pending "add some examples to (or delete) #{__FILE__}"
 
   # it "renders something useful" do
