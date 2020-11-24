208319ef914ccc45e03471f34cf5e7d022e35d73
r11n <nekkanti.raghavendra2@gmail.com>
2020-11-24 20:15:40 +0530

    add support for URL state push

---
+++ app/javascript/controllers/frontend/param_redirect.js
@@ -0,0 +1,48 @@
+export default function ParamRedirect({
+  param,
+  value,
+  turbolinks = true,
+  removeParams = [],
+  route = window.location.pathname,
+}) {
+  const params = locationParams();
+  params[param] = value;
+  removeParams.forEach((key) => {
+    delete params[key];
+  });
+  const query = new URLSearchParams(params).toString();
+  const url = `${route}?${query}`;
+  if (!turbolinks) {
+    window.location.assign(url);
+  } else {
+    Turbolinks.visit(url);
+  }
+}
+
+export function locationParams() {
+  const query = window.location.search;
+  const paramStore = new URLSearchParams(query);
+  const paramKeys = Array.from(paramStore.keys());
+  const params = {};
+  paramKeys.forEach((key) => {
+    const val = paramStore.getAll(key);
+    params[key] = val.length > 1 ? val : val[0];
+  });
+  return params;
+}
+
+export function URIPush({
+  param,
+  value,
+  removeParams = [],
+  route = window.location.pathname,
+}) {
+  const params = locationParams();
+  params[param] = value;
+  removeParams.forEach((key) => {
+    delete params[key];
+  });
+  const query = new URLSearchParams(params).toString();
+  const url = `${route}?${query}`;
+  window.history.pushState('', '', url);
+}
