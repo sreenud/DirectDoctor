07d93e2e5d7cc24aefe9027da6b049a207f8f970
sreenud <sreenud@outlook.com>
2020-12-03 20:58:25 +0530

    Profile page changes

--- app/components/star_rating_component.rb
+++ app/components/star_rating_component.rb
@@ -4,5 +4,8 @@ class StarRatingComponent < ViewComponent::Base
 
     @filled_stars = rating.to_f.round
     @empty_stars = 5 - @filled_stars
+
+    @height = @props[:height].present? ? @props[:height] : '4'
+    @width = @props[:width].present? ? @props[:width] : '4'
   end
 end
