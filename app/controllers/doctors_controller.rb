094f0bf0f6351013dc2718551e1621038615f0c4
sreenud <sreenud@outlook.com>
2020-12-07 12:27:35 +0530

    Started working on doctor profile dynamic

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -3,5 +3,7 @@ class DoctorsController < ApplicationController
     @doctor = Doctor.new
     @doctor.lat = 40.73061
     @doctor.lng = -73.935242
+
+    @doctor = Doctor.find_by_fdd_id(params[:fdd_id])
   end
 end
