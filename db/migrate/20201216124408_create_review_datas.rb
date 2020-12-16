49603d4cabea4eba77acf0ab0c564d2b8a9ae2bc
r11n <nekkanti.raghavendra2@gmail.com>
2020-12-16 19:13:57 +0530

    rubocop offenses fixed

--- db/migrate/20201216124408_create_review_datas.rb
+++ db/migrate/20201216124408_create_review_datas.rb
@@ -1,5 +1,5 @@
 class CreateReviewDatas < ActiveRecord::Migration[6.0]
   def change
-    create_view :review_datas, materialized: true
+    create_view(:review_datas, materialized: true)
   end
 end
