0439887c1cc29b0854e28d68125d35406afaf6d1
sreenud <sreenud@outlook.com>
2020-12-17 15:03:45 +0530

    Search and profile page design changes

--- app/models/review_data.rb
+++ app/models/review_data.rb
@@ -6,7 +6,7 @@ class ReviewData < ApplicationRecord
   end
 
   def self.refresh
-    Scenic.database.refresh_materialized_view(table_name, concurrently: true, cascade: false)
+    Scenic.database.refresh_materialized_view(table_name, concurrently: false, cascade: false)
   end
 
   def self.refresh!
