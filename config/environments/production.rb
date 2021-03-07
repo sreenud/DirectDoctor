cbde6ba626f342346039cd1f64ba4ff27578f61b
sreenud <sreenud@outlook.com>
2021-03-07 15:03:31 +0530

    Signup email and image cropping for provider is enabled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,5 +104,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'findmydirectdoctor.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'www.findmydirectdoctor.com', protocol: 'https' }
 end
