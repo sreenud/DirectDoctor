21964e02df26290a71566b54327f9bd930a9b734
sreenud <sreenud@outlook.com>
2021-04-07 13:25:02 +0530

    Not available option vanished issue fixed

--- app/controllers/concerns/doctor_form_data.rb
+++ app/controllers/concerns/doctor_form_data.rb
@@ -21,18 +21,18 @@ module DoctorFormData
     @states = State.by_name
 
     @patients_in_panel = Doctor.patients_in_panels
-    if current_user.has_role?(:admin) || current_user.has_role?(:data_entry)
-      @patients_in_panel = @patients_in_panel.merge!({ '0-0' => 'Not available' })
+    if current_user.has_role?('admin') || current_user.has_role?('data_entry')
+      @patients_in_panel = { '0-0' => 'Not available' }.merge!(@patients_in_panel)
     end
 
     @price_ranges = Doctor.price_ranges
-    if current_user.has_role?(:admin) || current_user.has_role?(:data_entry)
-      @price_ranges = @price_ranges.merge!({ '0-0' => 'Not available' })
+    if current_user.has_role?('admin') || current_user.has_role?('data_entry')
+      @price_ranges = { '0-0' => 'Not available' }.merge!(@price_ranges)
     end
 
     @experiences = Doctor.experiences
-    if current_user.has_role?(:admin) || current_user.has_role?(:data_entry)
-      @experiences = @experiences.merge!({ '0-0' => 'Not available' })
+    if current_user.has_role?('admin') || current_user.has_role?('data_entry')
+      @experiences = { '0-0' => 'Not available' }.merge!(@experiences)
     end
   end
 end
