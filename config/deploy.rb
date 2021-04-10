1b6ee953e50a4a917db52e27a6cf19627588fc5a
sreenud <sreenud@outlook.com>
2021-04-10 22:43:42 +0530

    device.rb file fetching social keys from secrets file

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,7 +54,7 @@ set(:assets_prefix, "packs")
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/secrets.yml.key"
+# append :linked_files, "config/secrets.yml.key"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
